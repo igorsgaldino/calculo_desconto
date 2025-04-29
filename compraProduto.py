05a26d232a56ee0692e63e36107ce1f03e394a6a
Igor Galdino <89166943+igorsgaldino@users.noreply.github.com>
2025-04-29 20:32:25 -0300

    Adicionando condicional elif

--- compraProduto.py
+++ compraProduto.py
@@ -9,13 +9,11 @@ qtd = int(input('Qual a quantidade?'))
 if (produto == 1):
     pagar = qtd * 2.3
     print(f'Você comprou {qtd} maçãs. O valor total a pagar: {pagar}')
+elif(produto == 2):
+    pagar = qtd * 3.6
+    print(f'Você comprou {qtd} laranjas. O valor total a pagar: {pagar}')   
+elif(produto == 3):
+    pagar = qtd * 1.85
+    print(f'Você comprou {qtd} maçãs. O valor total a pagar: {pagar}')
 else:
-    if (produto == 2):
-        pagar = qtd * 3.6
-        print(f'Você comprou {qtd} laranjas. O valor total a pagar: {pagar}')   
-    else:
-        if (produto == 3):
-            pagar = qtd * 1.85
-            print(f'Você comprou {qtd} maçãs. O valor total a pagar: {pagar}')
-        else:
-            print('Produto inexistente!')
+    print('Produto inexistente!')
